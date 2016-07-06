3363119fe2c4bcebb910bc69f1369caf3f879884
Junior <nog.junior84@gmail.com>
2016-07-06 10:00:54 -0300

    Validações Veiculos

--- app/models/veiculo.rb
+++ app/models/veiculo.rb
@@ -1,2 +1,7 @@
 class Veiculo < ActiveRecord::Base
+  belongs_to :user
+
+  validates :placa_veiculo, :tipo, :modelo, :cor, presence: true
+  validates_uniqueness_of :placa_veiculo
+  validates_format_of :placa_veiculo, with: /[A-Z]{3}-[0-9]{4}/i, create: :on
 end
