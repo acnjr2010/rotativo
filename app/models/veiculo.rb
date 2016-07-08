a7d41e28700a603b7dca18f5c1c30a4bfffdc3c0
Junior <nog.junior84@gmail.com>
2016-07-08 11:18:36 -0300

    View venda falta a finalização

--- app/models/veiculo.rb
+++ app/models/veiculo.rb
@@ -1,8 +1,17 @@
 class Veiculo < ActiveRecord::Base
+  before_save :veiculo_upcase
+
   belongs_to :user
   has_many :vendas
 
   validates :placa_veiculo, :tipo, :modelo, :cor, presence: true
   validates_uniqueness_of :placa_veiculo
   validates_format_of :placa_veiculo, with: /[A-Z]{3}-[0-9]{4}/i, create: :on
+
+  def veiculo_upcase
+    self.placa_veiculo.upcase!
+    self.modelo.upcase!
+    self.tipo.upcase!
+    self.cor.upcase!
+  end
 end
